30d833081759d6531b450cd994e655fe0eadf2c7
Pradhan <chrisonpradhan@gmail.com>
2022-04-11 12:07:53 +0530

    Delete comments

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-//Create Total supply of 100 million in counstructor.
+
 //The name of the token is  PillowToken and the symbol is PILW
 
 contract Token is ERC20 {
@@ -12,8 +12,7 @@ contract Token is ERC20 {
         string memory symbol,
         uint256 intialsupply
     ) ERC20(name, symbol) {
-        // 1 dollar = 100 cents i.e 1 token = 1 * (10 ** decimals)
-        // _mint(msg.sender, 100 * 10**uint(decimals()));
+        
         _mint(msg.sender, intialsupply);
     }
 }
